955dfc85d7845b0aeae873e73e4627f910a4d28d
Amr Salem <salem.amr241@gmail.com>
2022-11-21 16:59:49 -0500

    Initial commit

---
+++ src/components/backup.vue
@@ -0,0 +1,70 @@
+<v-flex xs12>
+    
+    <v-list-item
+        
+        >
+        <template v-slot:prepend="{  }">
+          <v-col md="8" >
+          Completion
+          </v-col>
+          <v-col md="8">
+            Title
+          </v-col>
+          <v-col md="8">
+           Deadline
+          </v-col>
+          <v-col md="8">
+      Delete
+          </v-col>
+          <v-col md="8">
+          Update
+      </v-col>
+       
+           
+          </template>
+  
+        </v-list-item>
+         </v-flex> 
+        <v-list select-strategy="multiple" class="pt-0" >
+  <div v-for="task in tasks" :key="task.id" tasks_prop.sync="tasks">
+      
+      <v-flex  >
+    <v-list-item
+        @click="doneTask(task.id)"
+        :class="{ 'bg-green ' : task.done}"
+        >
+        <template v-slot:prepend="{  }">
+          <v-col md="10"  >
+            <v-list-item-action start>
+              <v-checkbox-btn :model-value="task.done"></v-checkbox-btn>
+            </v-list-item-action>
+          </v-col>
+          <v-col md="10">
+            <v-list-item-title :class="{ ' text-decoration-line-through' : task.done}"> <text>{{ task.title }}</text></v-list-item-title>
+          </v-col>
+          <v-col md="10" max-width="200px">
+            <v-list-item-title :class="{ ' text-decoration-line-through' : task.done}"> <text>{{ task.Deadline }}</text></v-list-item-title>
+          </v-col>
+          <v-col md="10">
+            <v-list-item-action>
+            <v-btn 
+            @click.stop="deleteTask(task.id)"
+            icon>
+              <v-icon color="blue lighten-1">mdi-delete</v-icon>
+            </v-btn>
+          </v-list-item-action>
+      </v-col>
+          <v-col md="6">
+          <v-list-item-action>
+            <EditTask :id=task v-bind:tasks="tasks" v-if="!task.done"></EditTask>
+          </v-list-item-action>
+      </v-col>
+       
+           
+          </template>
+  
+        </v-list-item>
+         </v-flex> 
+        </div>
+     
+    </v-list>
---
+++ src/components/datePicker.vue
@@ -0,0 +1,38 @@
+<template>
+    <h1>date picker</h1>
+    <datepicker
+      class="picker"
+      v-model="selected"
+      :locale="locale"
+      :upperLimit="to"
+      :lowerLimit="from"
+      inputFormat="dd.MM.yyyy"
+    />
+  </template>
+  
+  <script>
+  import DatePicker from "vue3-datepicker";
+  import "vue3-datepicker/dist/vue3-datepicker.css";
+  import { defineComponent } from "vue";
+  import { en } from "date-fns/locale";
+  
+  export default defineComponent({
+    name: "App",
+    components: {
+      datepicker: DatePicker,
+    },
+    data() {
+      return {
+        selected: new Date(),
+        from: new Date(),
+        to: null,
+      };
+    },
+    computed: {
+      locale: () => en,
+    },
+  });
+  </script>
+  
+  <style>
+  </style>
